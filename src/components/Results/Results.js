0122071c0843cc24e1b00cdebff6ee62f5cec98e
drdevlin <drdevlin@fastmail.com>
2021-01-25 13:00:04 -0500

    Get <Results /> to change based on fetch status

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -3,9 +3,39 @@ import { connect } from 'react-redux';
 import './Results.css';
 
 function Results(props) {
+  
+  const modes = {
+    idle: null,
+    loading: <p role='status'>Hold on...</p>,
+    succeeded: (
+      <ul>
+        {props.results.map(el => <li key={el.id}>{el.title}</li>)}
+      </ul>
+    ),
+    failed: <p role='status'>{props.error}</p>
+  }
+
+  let content;
+  switch (props.fetchStatus) {
+    case 'idle':
+      content = modes.idle;
+      break;
+    case 'loading':
+      content = modes.loading;
+      break;
+    case 'succeeded':
+      content = modes.succeeded;
+      break;
+    case 'failed':
+      content = modes.failed;
+      break;
+    default:
+      content = null;
+  }
+
   return (
     <section className="Results" aria-label='Results'>
-      <p>{ props.results.length }</p>
+      {content}
     </section>
   );
 }
--- src/components/Results/Results.test.js
+++ src/components/Results/Results.test.js
@@ -5,10 +5,37 @@ import store from '../../store/store';
 import Results from './Results';
 
 describe('<Results />', () => {
-  it('displays some feedback', async () => {
+  it('shows nothing when idle', async () => {
+    store.dispatch({ type: 'UPDATE_FETCHSTATUS', fetchStatus: 'idle'});
     await render(<Provider store={store}><Results /></Provider>);
-    const text = screen.getByText(/./g);
+    const text = screen.queryByText(/./g);
 
-    expect(text).toBeTruthy();
+    expect(text).toBeNull();
+  });
+
+  it('shows loading status when loading', async () => {
+    store.dispatch({ type: 'UPDATE_FETCHSTATUS', fetchStatus: 'loading'});
+    await render(<Provider store={store}><Results /></Provider>);
+    const text = screen.queryByText('Hold on...');
+
+    expect(text).not.toBeNull();
+  });
+
+  it('shows results when search succeeded', async () => {
+    store.dispatch({ type: 'UPDATE_RESULTS', results: [{ id: 1, title: 'FindMe' }] });
+    store.dispatch({ type: 'UPDATE_FETCHSTATUS', fetchStatus: 'succeeded'});
+    await render(<Provider store={store}><Results /></Provider>);
+    const text = screen.queryByText('FindMe');
+
+    expect(text).not.toBeNull();
+  });
+
+  it('shows error message when search failed', async () => {
+    store.dispatch({ type: 'UPDATE_ERROR', error: 'FindMe'});
+    store.dispatch({ type: 'UPDATE_FETCHSTATUS', fetchStatus: 'failed'});
+    await render(<Provider store={store}><Results /></Provider>);
+    const text = screen.queryByText('FindMe');
+
+    expect(text).not.toBeNull();
   });
 });
