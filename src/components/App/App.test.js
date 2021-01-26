ce6ca162661b0ae8330d7af245d0832e645d7ae8
drdevlin <drdevlin@fastmail.com>
2021-01-26 12:46:57 -0500

    Refactor

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -4,6 +4,18 @@ import { Provider } from 'react-redux';
 import store from '../../store/store';
 import App from './App';
 
+// Mock out fetch
+import { fakeResponse } from '../../utils/fake-response';
+global.fetch = async () => {
+  return {
+    ok: true,
+    json: async () => {
+      return JSON.parse(fakeResponse);
+    }
+  };
+};
+
+
 describe('<App />', () => {
   it('provides some feedback', async () => {
     await render(<Provider store={store}><App /></Provider>);
--- src/components/Lookup/Lookup.test.js
+++ src/components/Lookup/Lookup.test.js
@@ -5,6 +5,26 @@ import store from '../../store/store';
 import Lookup from './Lookup';
 import Results from '../Results/Results';
 
+// Mock out fetch
+import { fakeResponse } from '../../utils/fake-response';
+global.fetch = async (url) => {
+  try {
+    if (url) {
+      return {
+        ok: true,
+        json: async () => {
+          return JSON.parse(fakeResponse);
+        }
+      };
+    } else {
+      throw new Error('Network Error');
+    }
+  } catch(error) {
+    return Promise.reject(error);
+  }
+};
+
+
 describe('<Lookup />', () => {
 
   it('displays form label', async () => {
@@ -14,15 +34,28 @@ describe('<Lookup />', () => {
     expect(label).not.toBeNull();
   });
 
-  it('displays result table after search', async () => {
+  it('displays error when search is empty', async () => {
     await render(<Provider store={store}><Lookup /><Results /></Provider>);
     const button = screen.getByText('Gimme');
 
     fireEvent.click(button);
 
-    const results = await screen.findByText('Title');
+    const results = await screen.findByText('Network Error');
 
     expect(results).not.toBeNull();
-  })
+  });
+
+  it('displays results when search is not empty', async () => {
+    await render(<Provider store={store}><Lookup /><Results /></Provider>);
+    const input = screen.getByLabelText('What we watchin?');
+    const button = screen.getByText('Gimme');
+    
+    fireEvent.change(input, { target: { value: 'anything' } });
+    fireEvent.click(button);
+
+    const results = await screen.findByText('Law.and.Order.SVU.S22E01.Guardians.and.Gladiators.720p.AMZN.WEBRip.DDP5.1.x264');
+
+    expect(results).not.toBeNull();
+  });
 
 });
